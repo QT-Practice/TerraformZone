6bdb20f4f380bf51397a3ce952434c153f1f08ce
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-05-31 09:11:12 +0530

    Added condtional creation

--- may2025/aws/network/variables.tf
+++ may2025/aws/network/variables.tf
@@ -9,7 +9,12 @@ variable "network_info" {
   type = object({
     name = string
     cidr = string
-    subnets = list(object({
+    private_subnets = list(object({
+      name = string
+      az   = string
+      cidr = string
+    }))
+    public_subnets = list(object({
       name = string
       az   = string
       cidr = string
@@ -18,15 +23,7 @@ variable "network_info" {
   default = {
     name = "nop"
     cidr = "10.0.0.0/16"
-    subnets = [{
-      name = "app1"
-      az   = "ap-south-1a"
-      cidr = "10.0.0.0/24"
-      }, {
-      name = "app2"
-      az   = "ap-south-1b"
-      cidr = "10.0.1.0/24"
-      }, {
+    private_subnets = [{
       name = "db1"
       az   = "ap-south-1a"
       cidr = "10.0.2.0/24"
@@ -34,6 +31,15 @@ variable "network_info" {
       name = "db2"
       az   = "ap-south-1b"
       cidr = "10.0.3.0/24"
+    }]
+    public_subnets = [{
+      name = "app1"
+      az   = "ap-south-1a"
+      cidr = "10.0.0.0/24"
+      }, {
+      name = "app2"
+      az   = "ap-south-1b"
+      cidr = "10.0.1.0/24"
     }]
   }
 
