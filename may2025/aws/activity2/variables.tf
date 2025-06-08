0fdb7f4faa5f73882f50f15b4cb007d12ece969e
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-06-08 08:32:49 +0530

    Added changes to create vpc with subnets, associate internet gateway and configure default rout table

--- may2025/aws/activity2/variables.tf
+++ may2025/aws/activity2/variables.tf
@@ -11,3 +11,35 @@ variable "default_tags" {
   }
 
 }
+
+variable "network_info" {
+  type = object({
+    name                 = string
+    cidr_block           = string
+    enable_dns_hostnames = bool
+  })
+  default = {
+    cidr_block           = "10.113.0.0/16"
+    name                 = "ntier"
+    enable_dns_hostnames = true
+  }
+
+}
+
+variable "subnets_info" {
+  type = list(object({
+    name              = string
+    cidr_block        = string
+    availability_zone = string
+  }))
+  default = [{
+    name              = "web1"
+    cidr_block        = "10.113.0.0/24"
+    availability_zone = "ap-south-1a"
+    }, {
+    name              = "web2"
+    cidr_block        = "10.113.1.0/24"
+    availability_zone = "ap-south-1b"
+  }]
+
+}
